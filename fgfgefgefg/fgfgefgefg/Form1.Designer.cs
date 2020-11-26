eff61cb3c253430505b1c8a7442f08adc3a2a695
Bondar Vladislav <72622783+Bondar-Vladislav@users.noreply.github.com>
2020-11-26 22:53:04 +0500

    Add files via upload

---
+++ fgfgefgefg/fgfgefgefg/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace fgfgefgefg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(560, 310);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(623, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            " ",
+            "1 - нерабочая",
+            "2",
+            "3",
+            "4 - нерабочая",
+            "5",
+            "6",
+            "7 -моя",
+            "8 -моя",
+            "9 -моя"});
+            this.comboBox1.Location = new System.Drawing.Point(603, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ fgfgefgefg/fgfgefgefg/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace fgfgefgefg
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class DLL1
+        {
+            private const string DLL = "Lib.dll";
+            [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)]
+            public static extern double TheFunc(double x);
+            [DllImport(DLL, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+            public static extern IntPtr FuncName();
+        }
+        class DLL2
+        {
+            private const string DLL = "Lib2-2-1.dll";
+            [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)]
+            public static extern double TheFunc(double x);
+            [DllImport(DLL, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+            public static extern IntPtr FuncName();
+        }
+        class DLL3
+        {
+            private const string DLL = "Lib2-2-2.dll";
+            [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)]
+            public static extern double TheFunc(double x);
+            [DllImport(DLL, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+            public static extern IntPtr FuncName();
+        }
+        class DLL4
+        {
+            private const string DLL = "Lib2-2-3-1.dll";
+            [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)]
+            public static extern double TheFunc(double x);
+            [DllImport(DLL, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+            public static extern IntPtr FuncName();
+        }
+        class DLL5
+        {
+            private const string DLL = "Lib2-2-3-2.dll";
+            [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)]
+            public static extern double TheFunc(double x);
+            [DllImport(DLL, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+            public static extern IntPtr FuncName();
+        }
+        class DLL6
+        {
+            private const string DLL = "Lib2-2-3.dll";
+            [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)]
+            public static extern double TheFunc(double x);
+            [DllImport(DLL, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+            public static extern IntPtr FuncName();
+        }
+        class DLL7
+        {
+            private const string DLL = "DLL1.dll";
+            [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)]
+            public static extern double TheFunc(double x);
+            [DllImport(DLL, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+            public static extern IntPtr FuncName();
+        }
+        class DLL8
+        {
+            private const string DLL = "DLL2.dll";
+            [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)]
+            public static extern double TheFunc(double x);
+            [DllImport(DLL, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+            public static extern IntPtr FuncName();
+        }
+        class DLL9
+        {
+            private const string DLL = "DLL3.dll";
+            [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)]
+            public static extern double TheFunc(double x);
+            [DllImport(DLL, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+            public static extern IntPtr FuncName();
+        }
+        class dll
+        {
+            public static List<Func<IntPtr>> ListFuncName = new List<Func<IntPtr>>
+            {
+                DLL1.FuncName,
+                DLL2.FuncName,
+                DLL3.FuncName,
+                DLL4.FuncName,
+                DLL5.FuncName,
+                DLL6.FuncName,
+                DLL7.FuncName,
+                DLL8.FuncName,
+                DLL9.FuncName
+
+            };
+            public static List<Func<double, double>> ListTheFunc = new List<Func<double, double>>
+            {
+                DLL1.TheFunc,
+                DLL2.TheFunc,
+                DLL3.TheFunc,
+                DLL4.TheFunc,
+                DLL5.TheFunc,
+                DLL6.TheFunc,
+                DLL7.TheFunc,
+                DLL8.TheFunc,
+                DLL9.TheFunc
+
+            };
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+             void Sch(int x)
+            {
+                var series = this.chart1.Series[0];
+                var fname = dll.ListFuncName.ElementAt(x -1);
+                var tfunc = dll.ListTheFunc.ElementAt(x -1);
+                double X = 1;
+                int N = 1;
+                while (N <= 100)
+                {
+                    
+                series.Points.AddXY(X, tfunc(X)); 
+                    
+                X += 0.1;
+                N++;
+                }
+            }
+
+            Sch(comboBox1.SelectedIndex);
+ 
+        }
+    }
+}
